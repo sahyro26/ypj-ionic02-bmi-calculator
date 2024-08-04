23d38b2de1aba260585b8370eaddff0841b162aa
sahyro <sahyro@genejunction.com>
2024-08-04 12:34:59 +0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  url: string =
+    "https://play-lh.googleusercontent.com/wiLKNn9tyY-SiNWg_Pib1stLmZU9lYuadq-Y4ASfi7OtJzLDmDknUFcqejQjw4Sqjg"
+  height: number = 150
+  weight: number = 50
+  bmi: number = 0
+  indocator: String = ''
+  constructor() { }
 
-  constructor() {}
+  calculateBMI() {
+    this.bmi = Math.round(this.weight / Math.pow((this.height / 100), 2))
+    if (this.bmi < 18.5) {
+      this.indocator = "Underweight"
+    }
+    else if (this.bmi < 25) {
+      this.indocator = "Overweight"
+    }
+    else if (this.bmi < 30) {
+      this.indocator = "Overweight"
+    }
+    else if (this.bmi < 40) {
+      this.indocator = "Obese"
+    }
+    else if (this.bmi > 40) {
+      this.indocator = "Morbidly Obese"
+    }
+  }
 
 }
